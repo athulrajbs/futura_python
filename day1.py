4be2fe5bcfe38327642edcdf6dd6a531ff1fc3c6
athulrajbs <athulrajbs309@gmail.com>
2022-04-25 14:04:11 +0530

    nested if aaded

--- day1.py
+++ day1.py
@@ -1,3 +1,28 @@
-a=int(input('enter a number'))
-b=int(input('enter a number'))
-10
+# a=int(input('enter a number'))
+# b=int(input('enter a number'))
+# c=a*b
+# print("numbers are",c)
+
+
+# if =else conditional statement
+# a=-1
+# if a>10:
+#
+#     print('True')
+# else:
+#     print("False")
+
+
+# nested if
+
+choice= int(input("enter your choice:"))
+if choice==1:
+    print("monday")
+elif choice==2:
+    print("tuesday")
+elif choice==3:
+    print("wednesday")
+elif choice==4:
+    print("thursday")
+else:
+    print("enter right choice")
